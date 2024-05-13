6feb5edec97190c7cf7a962e9236552595a79dae
Jiří Štengl <stengljirka@gmail.com>
2024-05-13 13:01:37 +0200

    Done single color mode

--- rgb_leds_handlers.py
+++ rgb_leds_handlers.py
@@ -1,8 +1,12 @@
 from neopixel import NeoPixel
-from machine import Pin
+from machine import Pin, ADC
 
 __RGB_LEDS_PIN: Pin = Pin(15, Pin.OUT)
 __NUMBER_OF_LEDS = 60
+__red_potentiometer = ADC(Pin(26, Pin.IN))
+__green_potentiometer = ADC(Pin(27, Pin.IN))
+__blue_potentiometer = ADC(Pin(28, Pin.IN))
+
 __leds = NeoPixel(__RGB_LEDS_PIN, __NUMBER_OF_LEDS)
 
 
@@ -13,8 +17,15 @@ def __convert_potentiometer_reading_to_color(value: int) -> int:
 
 
 def handle_single_color_mode() -> None:
-    print("Handling single color mode")
-    # TODO tady budeme programovat
+    red_potentiometer_value = __red_potentiometer.read_u16()
+    red = __convert_potentiometer_reading_to_color(red_potentiometer_value)
+    green_potentiometer_value = __green_potentiometer.read_u16()
+    green = __convert_potentiometer_reading_to_color(green_potentiometer_value)
+    blue_potentiometer_value = __blue_potentiometer.read_u16()
+    blue = __convert_potentiometer_reading_to_color(blue_potentiometer_value)
+    for i in range(__NUMBER_OF_LEDS):
+        __leds[i] = (green, red, blue)
+    __leds.write()
 
 
 def handle_snake_mode() -> None:
