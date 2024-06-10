7d076c976b253d4482d809a95eb83f29de2510b6
Jiří Štengl <stengljirka@gmail.com>
2024-06-10 16:32:23 +0200

    Added hsv to rgb function

--- rgb_leds_handlers.py
+++ rgb_leds_handlers.py
@@ -9,6 +9,9 @@ __blue_potentiometer = ADC(Pin(28, Pin.IN))
 
 __leds = NeoPixel(__RGB_LEDS_PIN, __NUMBER_OF_LEDS)
 
+__MAX_COLOR_VALUE = 255
+HUE_PERIOD = 360.0
+
 __SNAKE_LENGTH = 10
 __SNAKE_SPEED_COEFFICIENT = 3
 __snake_head_position: int = __SNAKE_LENGTH - 1
@@ -99,3 +102,37 @@ def __draw_snake() -> None:
         for i in range(__NUMBER_OF_LEDS - 1, -1, -1):
             coefficient += write_to_led(i, __snake_head_position, end)
     __leds.write()
+
+
+def hsv_to_rgb(h: float, s: float, v: float) -> (int, int, int):
+    __validate_hsv_color(s, v)
+    modulated_h = h % HUE_PERIOD
+    c = v * s
+    x = c * (1 - abs((modulated_h / 60) % 2 - 1))
+    m = v - c
+    r_coef, g_coef, b_coef = __calculate_rgb_coefficients(modulated_h, c, x)
+    r = (r_coef + m) * __MAX_COLOR_VALUE
+    g = (g_coef + m) * __MAX_COLOR_VALUE
+    b = (b_coef + m) * __MAX_COLOR_VALUE
+    return int(r), int(g), int(b)
+
+
+def __calculate_rgb_coefficients(h: float, c: float, x: float) -> (float, float, float):
+    if h < 60:
+        return c, x, 0
+    elif h < 120:
+        return x, c, 0
+    elif h < 180:
+        return 0, c, x
+    elif h < 240:
+        return 0, x, c
+    elif h < 300:
+        return x, 0, c
+    return c, 0, x
+
+
+def __validate_hsv_color(s: float, v: float) -> None:
+    if s > 1.0 or s < 0:
+        raise ValueError(f"Saturation value ({s}) is not in range from 0 to 1")
+    if v > 1.0 or v < 0:
+        raise ValueError(f"Value value ({v}) is not in range from 0 to 1")
